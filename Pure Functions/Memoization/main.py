ba32e5b7d020d56db779059e7366bd5fc535c7fa
ebagabee <ebagabe.2025@gmail.com>
2025-12-07 09:23:10 -0300

    feat: memoization test

---
+++ Pure Functions/Memoization/main.py
@@ -0,0 +1,13 @@
+def word_count_memo(document, memos):
+    memos_copy = memos.copy()
+
+    if document in memos_copy:
+        return memos_copy[document], memos_copy
+
+    memos_copy[document] = word_count(document)
+    return memos_copy[document], memos_copy
+    
+# Don't edit below this line
+def word_count(document):
+    count = len(document.split())
+    return count
