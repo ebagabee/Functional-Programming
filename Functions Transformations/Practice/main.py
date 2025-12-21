28cb03794751623b5d25a4538f404bf0cdf47801
ebagabee <gh.alvesantos@gmail.com>
2025-12-21 14:33:42 -0300

    feat: enhance get_filter_cmd to handle multiple options in filter_cmd function

--- Functions Transformations/Practice/main.py
+++ Functions Transformations/Practice/main.py
@@ -1,9 +1,19 @@
 def get_filter_cmd(filter_one, filter_two):
-    def filter_cmd(content, option):
-        pass
+    def filter_cmd(content, option = "--one"):
+        if option == "--one":
+            return filter_one(content)
+
+        if option == "--two":
+            return filter_two(content)
+
+        if option == "--three":
+            result_one = filter_one(content)
+
+            return filter_two(result_one)
+
+        raise Exception("invalid option")
 
     return filter_cmd
-
 
 def replace_bad(text):
     return text.replace("bad", "good")
